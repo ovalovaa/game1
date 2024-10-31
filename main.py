8196e7b8b1c0974e1e47fdf7ea98d12a758bf429
ovalova <ovalova@kse.org.ua>
2024-10-31 15:49:18 +0200

    added vampire

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ lives_bot = 50
 
 for attack in range (5):
     print(f"{attack+1} attack")
-    action = input("Choose your action: a) attack  b) healing")
+    action = input("Choose your action: a) attack  b) healing c) VAMPIRE")
     if action == "a":
         kill_bot = random.randint (5,15)
         lives_bot = lives_bot-kill_bot
@@ -14,11 +14,16 @@ for attack in range (5):
     elif action == "b":
         lives_human = lives_human+random.randint (5,10)
         print("you successfully healed, your total is: ", lives_human)
+    elif action == "c":
+        lives_human = lives_human+random.randint(7, 18)
+        lives_bot = lives_bot-random.randint(7,18)
+        print(f"YOU BIT HIM, his lives are: , {lives_bot}")
+        print(f"you healed yourself, your total is: {lives_human}")
     if lives_bot <= 0:
         print("Game over, you won! ")
         break
     #bots move
-    kill_human = random.randint (5,20)
+    kill_human = random.randint (10,30)
     lives_human = lives_human - kill_human
     print("enemy has attacked you! Your total is: ", lives_human)
     if lives_human <= 0:
